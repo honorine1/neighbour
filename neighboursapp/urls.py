c9ca84fed0d3d76e26e272f252f4b003c2f17eb5
honorine1 <uwurukundoh@gmail.com>
2019-11-04 10:13:53 +0200

    urls.pyis added

--- neighboursapp/urls.py
+++ neighboursapp/urls.py
@@ -5,6 +5,10 @@ from django.conf import settings
 
 urlpatterns=[
     url('^$',views.index,name = 'index'),
+   
+    url(r'^project_posts/', views.project_posts, name='project_posts'),
+    url(r'^viewProject_details/', views.viewProject_details, name='viewProject_details'),
+    url(r'^new_post/',views.new_post,name='new_post'),
     url(r'^profile/(\d+)',views.profile,name = 'profile'),
     url(r'^update_profile/',views.update_profile,name = 'update_profile'),
 ]
