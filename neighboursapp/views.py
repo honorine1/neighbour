e575365e0c965a9ee53c0fec137992edfd7f13c3
honorine1 <uwurukundoh@gmail.com>
2019-11-04 10:14:23 +0200

    views.pyis added

--- neighboursapp/views.py
+++ neighboursapp/views.py
@@ -6,7 +6,50 @@ from .forms import ProfileForm,PostForm
 # Create your views here.
 
 def index(request):
-    return render(request, 'neighbour/index.html')
+    current_user = request.user
+    images= Post.objects.all().order_by("posted_date")
+    profile= Profile.objects.all()
+    
+    return render(request,'neighbour/index.html',{"images":images,"profile":profile})
+
+    # return render(request, 'neighbour/index.html')
+
+@login_required(login_url='/accounts/login/')
+def viewProject_details(request):
+    current_user = request.user
+    image= Post.objects.all().order_by("posted_date")
+    
+    profile= Profile.objects.all()
+    
+    return render(request,'neighbour/post.html',{"images":image,"profile":profile})
+
+
+@login_required(login_url='/accounts/login/')
+def project_posts(request, post_id):
+    try:
+        posts = Post.objects.all().order_by('posted_date')
+       
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"neighbour/post_post.html", {"posts":posts})
+
+
+@login_required(login_url='/accounts/login/')
+def new_post(request):
+    current_user = request.user
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            projects = form.save(commit=False)
+            projects.user = current_user
+            projects.save()
+            return redirect('index')
+
+    else:
+        form = PostForm()
+    return render(request, 'neighbour/post_form.html', {"form": form})
+
 
 def profile(request,user_id ):
 
@@ -24,12 +67,12 @@ def profile(request,user_id ):
 
     user=User.objects.all()
 
-    # proImage = Projects.objects.filter(user__username=current_user)
+    post = Post.objects.filter(user__username=current_user)
 
 
     profile = Profile.objects.filter(user__username = current_user)
 
-    return render(request,"neighbour/profile.html",{"user":user,"profile":profile})
+    return render(request,"neighbour/profile.html",{"user":user,"profile":profile,"post":post})
 
 @login_required(login_url='/accounts/login/')
 def update_profile(request):
