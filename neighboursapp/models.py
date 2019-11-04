db16087668222198cf6ff503f7ad9fc8f6ea1733
honorine1 <uwurukundoh@gmail.com>
2019-11-04 10:13:31 +0200

    models.pyis added

--- neighboursapp/models.py
+++ neighboursapp/models.py
@@ -7,11 +7,11 @@ import datetime as dt
 # Create your models here.
 
 class Profile(models.Model):
-    profile_pic =  models.ImageField(default='default.jpg',upload_to = 'profile_pics/', blank=True, null=True) 
-    bio = models.TextField(max_length = 200,null=True)
-    neighbourhoodName = models.TextField(max_length = 200,null=True)
-    contact  = models.IntegerField(default=None,null=True)
-    general_loc = models.CharField(max_length = 50,null=True)
+    profile_pic =  models.ImageField(default='default.jpg',upload_to = 'profile_pics/', blank=True) 
+    bio = models.TextField(max_length = 200)
+    neighbourhoodName = models.TextField(max_length = 200)
+    contact  = models.IntegerField(default=None)
+    general_loc = models.CharField(max_length = 50)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -19,9 +19,9 @@ class Profile(models.Model):
 
 class Post(models.Model):
     
-    image =  models.ImageField(upload_to = 'post/', blank=True)
+    image =  models.ImageField(upload_to = 'post_pics/', blank=True)
     desc = models.TextField(max_length=200)
-    profile = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     posted_date = models.DateTimeField(auto_now_add=True)
 
